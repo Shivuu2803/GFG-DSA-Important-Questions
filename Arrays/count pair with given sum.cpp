68990dbb45b0495113ba5f5c9e18b6386ff1961a
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-12-15 11:50:05 +0530

    Create count pair with given sum.cpp

---
+++ Arrays/count pair with given sum.cpp
@@ -0,0 +1,22 @@
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) 
+    {
+        unordered_map<int, int> frequency;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) 
+        {
+            int diff = k - arr[i];
+
+            if (frequency.find(diff) != frequency.end()) 
+            {
+                count += frequency[diff];
+            }
+            
+            frequency[arr[i]]++;
+        }
+
+        return count;
+    }
+};
