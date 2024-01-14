4a35437b3ce7a8a11c1f690bdd0b3eca0fe87d29
Shivansh Mishra <94588491+Shivuu2803@users.noreply.github.com>
2024-01-14 22:50:43 +0530

    Create Next greater element.cpp

---
+++ Stack/Next greater element.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
+    {
+        vector<long long> result(n, -1); 
+        stack<int> s;
+
+        for(int i = 0; i < n; i++) 
+        {
+            while (!s.empty() && arr[i] > arr[s.top()]) 
+            {
+                result[s.top()] = arr[i]; 
+                s.pop(); 
+            }
+
+            s.push(i); 
+        }
+
+        return result;
+    }
+};
